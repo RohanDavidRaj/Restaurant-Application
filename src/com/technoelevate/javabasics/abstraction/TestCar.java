f89063226a254938791beee0716ff7d4da543325
Rohan David Raj <54152357+RohanDavidRaj@users.noreply.github.com>
2022-10-10 12:28:10 +0530

    Delete src/com/technoelevate/javabasics/abstraction directory

--- src/com/technoelevate/javabasics/abstraction/TestCar.java
+++
@@ -1,9 +0,0 @@
-package com.technoelevate.javabasics.abstraction;
-
-public class TestCar {
-
-	public static void main(String[] args) {
-		Car car = CarFactory.getCar();
-		Driver.drive(car);
-	}
-}
