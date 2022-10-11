207363990722dc48178b9c7f057fcdf784729a19
Rohan David Raj <54152357+RohanDavidRaj@users.noreply.github.com>
2022-10-11 11:39:42 +0530

    Delete src/com/technoelevate/javabasics/exception directory

--- src/com/technoelevate/javabasics/exception/BalanceException.java
+++
@@ -1,8 +0,0 @@
-package com.technoelevate.javabasics.exception;
-
-public class BalanceException extends RuntimeException {
-
-	public BalanceException() {
-		super("Balance is incufficient");
-	}
-}
